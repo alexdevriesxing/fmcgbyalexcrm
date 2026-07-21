f0c7feff6427abf12f380b6b8a0d2119fd69d16d
alexdevriesxing <alexdevriesxing@gmail.com>
2026-07-22 00:53:13 +0200

    feat: register commercial API routes

--- apps/api/src/worker.ts
+++ apps/api/src/worker.ts
@@ -1,6 +1,8 @@
 import app from './index';
 import { registerBusinessRoutes } from './business-routes';
+import { registerCommercialRoutes } from './commercial-routes';
 
 registerBusinessRoutes(app);
+registerCommercialRoutes(app);
 
 export default app;
