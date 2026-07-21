81a5c4beb32abca6feee08a281711db4abbd6f52
alexdevriesxing <alexdevriesxing@gmail.com>
2026-07-21 23:25:52 +0200

    test(api): migrate control and tenant databases

--- apps/api/test/setup.ts
+++ apps/api/test/setup.ts
@@ -5,7 +5,10 @@ type MigrationList = Parameters<typeof applyD1Migrations>[1];
 
 const testEnv = env as unknown as {
   CONTROL_DB: D1Database;
-  TEST_MIGRATIONS: MigrationList;
+  TENANT_DB: D1Database;
+  TEST_CONTROL_MIGRATIONS: MigrationList;
+  TEST_TENANT_MIGRATIONS: MigrationList;
 };
 
-await applyD1Migrations(testEnv.CONTROL_DB, testEnv.TEST_MIGRATIONS);
+await applyD1Migrations(testEnv.CONTROL_DB, testEnv.TEST_CONTROL_MIGRATIONS);
+await applyD1Migrations(testEnv.TENANT_DB, testEnv.TEST_TENANT_MIGRATIONS);
